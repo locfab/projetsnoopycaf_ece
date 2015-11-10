7b0601fae0b91f3c963073ea6ce43faac82944d2
thefab96 <locqueville.fabrice@gmail.com>
2015-11-10 22:13:41 +0100

    modification du .h de la balle avec rajout de const au getter et rajout de depX, depY et ajout et suppression de methodes

--- Snoppy/Balle.h
+++ Snoppy/Balle.h
@@ -6,17 +6,24 @@ class Balle
     public:
     Balle();
     Balle(int x, int y);
-    void deplacement(int x, int y);
-    void setCoordonnees(int x, int y);
-    void setX(int x);
-    void setY(int y);
-    int getX();
-    int getY();
+
+    void directionDuDeplacementSuivant(bool droite, bool haut); // modification de m_depX et m_depY
+    void deplacement(); // modification de m_x et m_y
+
+    void setX();
+    void setY();
+    void setDepX();
+    void setDepY();
+
+    int getX() const;
+    int getY() const;
 
     protected:
 
     int m_x;
     int m_y;
+    int depx;
+    int depy;
     char m_saLetrre = 'B';
 };
 
